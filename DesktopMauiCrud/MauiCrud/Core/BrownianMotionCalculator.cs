55e37b721811ef459c7369bc0d3a75b485764da3
Diego Rocha <diegobarbosarocha@gmail.com>
2025-09-27 17:18:58 -0300

    feat: class for brownian motion calculator

---
+++ DesktopMauiCrud/MauiCrud/Core/BrownianMotionCalculator.cs
@@ -0,0 +1,34 @@
+﻿using DesktopMauiCrud.MauiCrud.Core.Exceptions;
+
+namespace DesktopMauiCrud.MauiCrud.Core
+{
+    public class BrownianMotionCalculator
+    {
+        public double[] GenerateBrownianMotion(double volatilityPercentage, double desiredAverageReturn,
+            double initialPrice, int numDays)
+        {
+            if (numDays < 2)
+            {
+                InvalidFlowException.Raise();
+            }
+
+            Random rand = new();
+
+            double[] prices = new double[numDays];
+            prices[0] = initialPrice;
+
+            for (int i = 1; i < numDays; i++)
+            {
+                double sample1 = 1.0 - rand.NextDouble();
+                double sample2 = 1.0 - rand.NextDouble();
+                double dayRandVariance = Math.Sqrt(-2.0 * Math.Log(sample1)) * Math.Cos(2.0 * Math.PI * sample2);
+
+                double retornoDiario = desiredAverageReturn + volatilityPercentage * dayRandVariance;
+
+                prices[i] = prices[i - 1] * Math.Exp(retornoDiario);
+            }
+
+            return prices;
+        }
+    }
+}
---
+++ DesktopMauiCrud/MauiCrud/Core/Exceptions/InvalidFlowException.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace DesktopMauiCrud.MauiCrud.Core.Exceptions
+{
+    internal class InvalidFlowException : CustomException
+    {
+        protected InvalidFlowException(string message) : base(message)
+        {
+        }
+
+        [DoesNotReturn]
+        public static void Raise()
+        {
+            throw new InvalidFlowException("Invalid flow exception");
+        }
+    }
+}
