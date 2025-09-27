af677e6898c93ceb33e7bd9d5861c94d84e18204
Diego Rocha <diegobarbosarocha@gmail.com>
2025-09-27 17:20:08 -0300

    feat: dependencies and DI updates

--- DesktopMauiCrud/MauiProgram.cs
+++ DesktopMauiCrud/MauiProgram.cs
@@ -1,10 +1,14 @@
-﻿using DesktopMauiCrud.MauiCrud.Data.DAOs;
+﻿using DesktopMauiCrud.MauiCrud.Core;
+using DesktopMauiCrud.MauiCrud.Data.DAOs;
 using DesktopMauiCrud.MauiCrud.Data.Imps;
 using DesktopMauiCrud.MauiCrud.Data.Interface;
 using DesktopMauiCrud.MauiCrud.Data.Interface.UseCase;
+using DesktopMauiCrud.MauiCrud.Screens.Alerts;
 using DesktopMauiCrud.MauiCrud.Services;
 using DesktopMauiCrud.MauiCrud.ViewModels;
+using DesktopMauiCrud.MauiCrud.ViewModels.Simulation;
 using Microsoft.Extensions.Logging;
+
 
 namespace DesktopMauiCrud
 {
@@ -22,13 +26,20 @@ namespace DesktopMauiCrud
                 });
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
+            //SINGLETONS
             builder.Services.AddSingleton(typeof(IDataStorage<>), typeof(JsonDataStorage<>));
             builder.Services.AddSingleton<IClientDAO, ClientDAO>();
             builder.Services.AddSingleton<ClientService>();
+            builder.Services.AddSingleton<IAlertService, AlertService>();
+
+
+            //TRANSIENTS
             builder.Services.AddTransient<ClientListViewModel>();
             builder.Services.AddTransient<ClientEditViewModel>();
+            builder.Services.AddTransient<BrownianSimulationViewModel>();
+            builder.Services.AddTransient<BrownianMotionCalculator>();
 
             return builder.Build();
         }
