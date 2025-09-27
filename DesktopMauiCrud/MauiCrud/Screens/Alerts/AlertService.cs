a30263e74ce20e4b36608555e53d71ab476f3779
Diego Rocha <diegobarbosarocha@gmail.com>
2025-09-27 17:17:36 -0300

    feat: new interface for displaying alerts

---
+++ DesktopMauiCrud/MauiCrud/Screens/Alerts/AlertService.cs
@@ -0,0 +1,18 @@
+﻿namespace DesktopMauiCrud.MauiCrud.Screens.Alerts
+{
+    internal class AlertService : IAlertService
+    {
+        private Page CurrentPage => Shell.Current?.CurrentPage
+                                    ?? Application.Current?.MainPage
+                                    ?? throw new InvalidOperationException("No page available");
+
+        public Task ShowMessage(string message, string title = "Info", string cancel = "OK") =>
+            CurrentPage.DisplayAlert(title, message, cancel);
+
+        public Task<bool> ShowConfirmation(string message, string title = "Confirm", string accept = "Yes", string cancel = "No") =>
+            CurrentPage.DisplayAlert(title, message, accept, cancel);
+
+        public Task ShowError(string message, string title = "Error") =>
+            CurrentPage.DisplayAlert(title, message, "Close");
+    }
+}
---
+++ DesktopMauiCrud/MauiCrud/Screens/Alerts/IAlertService.cs
@@ -0,0 +1,9 @@
+﻿namespace DesktopMauiCrud.MauiCrud.Screens.Alerts
+{
+    public interface IAlertService
+    {
+        Task ShowMessage(string message, string title = "Info", string cancel = "OK");
+        Task<bool> ShowConfirmation(string message, string title = "Confirm", string accept = "Yes", string cancel = "No");
+        Task ShowError(string message, string title = "Error");
+    }
+}
