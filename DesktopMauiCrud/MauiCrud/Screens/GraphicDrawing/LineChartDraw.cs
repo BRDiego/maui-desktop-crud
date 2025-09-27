720eeb58bb599ea78fb13e37b1789adcf6ec337b
Diego Rocha <diegobarbosarocha@gmail.com>
2025-09-27 17:19:27 -0300

    feat: class for drawing the line chart

---
+++ DesktopMauiCrud/MauiCrud/Screens/GraphicDrawing/LineChartDraw.cs
@@ -0,0 +1,38 @@
+﻿namespace DesktopMauiCrud.MauiCrud.Screens.GraphicDrawing
+{
+    public class LineChartDraw : IDrawable
+    {
+        private double[] _dataArray;
+
+        public LineChartDraw(double[] data) 
+            => _dataArray = data;
+
+        public void UpdateData(double[] data) => _dataArray = data;
+        public void Draw(ICanvas canvas, RectF dirtyRect)
+        {
+            if (_dataArray.Length < 2) return;
+
+            float width = dirtyRect.Width;
+            float height = dirtyRect.Height;
+            float stepX = width / (_dataArray.Length - 1);
+            double maxY = _dataArray.Max();
+
+            canvas.StrokeColor = Colors.Blue;
+            canvas.StrokeSize = 3;
+
+            var path = new PathF();
+            for (int i = 0; i < _dataArray.Length; i++)
+            {
+                float x = i * stepX;
+                float y = height - (float)(_dataArray[i] / maxY * height);
+
+                if (i == 0)
+                    path.MoveTo(x, y);
+                else
+                    path.LineTo(x, y);
+            }
+
+            canvas.DrawPath(path);
+        }
+    }
+}
